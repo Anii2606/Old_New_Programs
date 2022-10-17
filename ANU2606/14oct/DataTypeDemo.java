505734ece559a85e6f38b2fa5c90dfc5d9c804ad
Anikesh Chouhan <115870421+Anii2606@users.noreply.github.com>
2022-10-17 18:15:11 +0530

    Add files via upload

---
+++ ANU2606/14oct/DataTypeDemo.java
@@ -0,0 +1,25 @@
+/*
+Program: To understand various data types in java
+@author: Anikesh Chouhan
+@Data: 17 October,2022
+*/
+
+//declaring a class
+class DataTypeDemo
+{
+	// main method
+	public static void main(String args[])
+	{
+		//declaring local veriables 
+		int number1= 75.5f;
+		int number2= 65;
+		
+		//calculating the sum
+		int sum = number1+ number2;
+		System.out.println("The sum is :"+sum); // println the value of sum
+	}
+	// end the class
+		
+		
+}
+// end of class DataTypeDemo
---
+++ ANU2606/14oct/DataTypeDemo2.java
@@ -0,0 +1,28 @@
+/*
+Program: To understand various data types in java
+@author: Anikesh Chouhan
+@Data: 17 October,2022
+*/
+
+//declearing a class
+class  DataTypeDemo2
+{
+	//main method
+	public static void main(String args[])
+	{
+		//declaring variables
+		String name= "Anikesh Chouhan";
+		char gender= 'M';
+		int age= 22;
+		float marks= 77.4f;
+		boolean pass= true;
+		
+		System.out.println("Student Name:" +name);//printing it
+		System.out.println("Gender:" +gender);
+		System.out.println("Age:" +age);
+		System.out.println("Marks:" +marks);
+		System.out.println("Pass or Fail:" +pass);
+	}
+	//end of main
+}
+//end of DataTypeDemo2
